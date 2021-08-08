1616eae3b722aaf49c986fb87a9dfcb7d9f55e5a
c0d3p0is0n3r <35402359+c0d3p0is0n3r@users.noreply.github.com>
2021-08-08 16:24:25 +0530

    Update port-scanner.py

--- port-scanner.py
+++ port-scanner.py
@@ -1,48 +1,4 @@
 #!/usr/bin/env python
-"""
-
-
-sock = socket.socket (socket_family, socket_type)
-Syntax for creating a socket
-
-sock = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
-Creates a stream socket
-
-AF_INET
-Socket Family (here Address Family version 4 or IPv4)
-
-SOCK_STREAM
-Socket type TCP connections
-
-SOCK_DGRAM
-Socket type UDP connections
-
-gethostbyname("host")
-Translate a host name to IPv4 address format
-
-socket.gethostbyname_ex("host")
-Translate a host name to IPv4 address format, extended interface
-
-socket.getfqdn("8.8.8.8")
-Get the fqdn (fully qualified domain name)
-
-socket.gethostname()
-Returns the hostname of the machine..
-
-socket.error
-Exception handling
-Making a program using Python Sockets
-How to make a simple port scanner program in Python
-
-This small port scanner program will try to connect on every port you define for
-a particular host.
-
-The first thing we must do is import the socket library and other libraries that
-we need.
-
-Open up an text editor, copy & paste the code below. Save the file as:
-"portscanner.py" and exit the editor
-"""
 
 import socket
 import subprocess
