d35d4a1610e50bf2660cec795f3182ebd4930fc5
0xmani <35402359+0xmani@users.noreply.github.com>
2024-03-26 13:36:12 +0530

    Update port-scanner.py
    Concurrent port scanner and faster

--- port-scanner.py
+++ port-scanner.py
@@ -1,54 +1,42 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import socket
 import subprocess
 import sys
 from datetime import datetime
+import concurrent.futures
 
-# Clear the screen
-subprocess.call('clear', shell=True)
+def scan_port(port):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(0.5)
+        result = sock.connect_ex((remoteServerIP, port))
+        if result == 0:
+            print("{}/tcp Open".format(port))
+        sock.close()
+    except Exception as e:
+        pass
 
-# Ask for input
-remoteServer = input("Enter a remote host to scan: ")
+
+if len(sys.argv) != 2:
+    print("Usage: {} <remote host>".format(sys.argv[0]))
+    sys.exit(1)
+
+remoteServer = sys.argv[1]
 remoteServerIP = socket.gethostbyname(remoteServer)
 
-# Print a nice banner with information on which host we are about to scan
+
 print("-" * 60)
 print("Please wait, scanning remote host", remoteServerIP)
 print("-" * 60)
 
-# Check what time the scan started
+
+with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
+    results = executor.map(scan_port, range(1, 65535))
+
 t1 = datetime.now()
-
-# Using the range function to specify ports (here it will scans all ports between 1 and 1024)
-
-# We also put in some error handling for catching errors
-
-try:
-    for port in range(1, 1025):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        result = sock.connect_ex((remoteServerIP, port))
-        if result == 0:
-            print("Port {}: 	 Open".format(port))
-        sock.close()
-
-except KeyboardInterrupt:
-    print("You pressed Ctrl+C")
-    sys.exit()
-
-except socket.gaierror:
-    print('Hostname could not be resolved. Exiting')
-    sys.exit()
-
-except socket.error:
-    print("Couldn't connect to server")
-    sys.exit()
-
-# Checking the time again
 t2 = datetime.now()
-
-# Calculates the difference of time, to see how long it took to run the script
 total = t2 - t1
 
-# Printing the information to screen
+
 print('Scanning Completed in: ', total)
